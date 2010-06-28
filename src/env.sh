bb6d329f04d527ee6792dd81fa4943f95b24e077
Bob Sherbert <bob@tyr.(none)>
2010-06-29 08:43:55 +0900

    unified mini and full hubo naming convention within model

--- src/env.sh
+++ src/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-LD_LIBRARY_PATH=/home/bob/thesis/support/lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=/home/bob/thesis/support/lib:/usr/local/webots/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 
--- src/wbtest.cpp
+++ src/wbtest.cpp
@@ -34,71 +34,88 @@ int ORO_main(int a, char** b){
         dispatch, 5, 31, 0.0, -1.0 );
 
     ACES::Parameter* HY = (ACES::Parameter*) new
-        Webots::Parameter("HY", "Waist_1",
+        //Webots::Parameter("HY", "Waist_1",
+        Webots::Parameter("HY", "HY",
         dispatch, 5, 31, 0.0, -1.0 );
 
     ACES::Parameter* LSP = (ACES::Parameter*) new
-        Webots::Parameter("LSP", "Shoulder_Elbow_2",
+        //Webots::Parameter("LSP", "Shoulder_Elbow_2",
+        Webots::Parameter("LSP", "LSP",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* LSR = (ACES::Parameter*) new
-        Webots::Parameter("LSR", "Bicep_2",
+        //Webots::Parameter("LSR", "Bicep_2",
+        Webots::Parameter("LSR", "LSR",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* RSP = (ACES::Parameter*) new
-        Webots::Parameter("RSP", "Shoulder_Elbow_1",
+        //Webots::Parameter("RSP", "Shoulder_Elbow_1",
+        Webots::Parameter("RSP", "RSP",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* RSR = (ACES::Parameter*) new
-        Webots::Parameter("RSR", "Bicep_1",
+        //Webots::Parameter("RSR", "Bicep_1",
+        Webots::Parameter("RSR", "RSR",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* LHY = (ACES::Parameter*) new
-        Webots::Parameter("LHY", "Hip_yaw_2",
+        //Webots::Parameter("LHY", "Hip_yaw_2",
+        Webots::Parameter("LHY", "LHY",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* LHR = (ACES::Parameter*) new
-        Webots::Parameter("LHR", "Hip_Ankle_pitch_roll_2",
+        //Webots::Parameter("LHR", "Hip_Ankle_pitch_roll_2",
+        Webots::Parameter("LHR", "LHR",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* LHP = (ACES::Parameter*) new
-        Webots::Parameter("LHP", "Thigh_2",
+        //Webots::Parameter("LHP", "Thigh_2",
+        Webots::Parameter("LHP", "LHP",
         dispatch, 5, 31, 0.0, -1.0 );
 
     ACES::Parameter* LKP = (ACES::Parameter*) new
-        Webots::Parameter("LKP", "Shin_2",
+        //Webots::Parameter("LKP", "Shin_2",
+        Webots::Parameter("LKP", "LKP",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* LAP = (ACES::Parameter*) new
-        Webots::Parameter("LAP", "Hip_Ankle_pitch_roll_4",
+        //Webots::Parameter("LAP", "Hip_Ankle_pitch_roll_4",
+        Webots::Parameter("LAP", "LAP",
         dispatch, 5, 31, 0.0, -1.0 );
 
     ACES::Parameter* LAR = (ACES::Parameter*) new
-        Webots::Parameter("LAR", "Foot_2",
+        //Webots::Parameter("LAR", "Foot_2",
+        Webots::Parameter("LAR", "LAR",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* RHY = (ACES::Parameter*) new
-        Webots::Parameter("RHY", "Hip_yaw_1",
+        //Webots::Parameter("RHY", "Hip_yaw_1",
+        Webots::Parameter("RHY", "RHY",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* RHR = (ACES::Parameter*) new
-        Webots::Parameter("RHR", "Hip_Ankle_pitch_roll_1",
+        //Webots::Parameter("RHR", "Hip_Ankle_pitch_roll_1",
+        Webots::Parameter("RHR", "RHR",
         dispatch, 5, 31, 0.0, -1.0 );
 
     ACES::Parameter* RHP = (ACES::Parameter*) new
-        Webots::Parameter("RHP", "Thigh_1",
+        //Webots::Parameter("RHP", "Thigh_1",
+        Webots::Parameter("RHP", "RHP",
         dispatch, 5, 31, 0.0, -1.0 );
 
     ACES::Parameter* RKP = (ACES::Parameter*) new
-        Webots::Parameter("RKP", "Shin_1",
+        //Webots::Parameter("RKP", "Shin_1",
+        Webots::Parameter("RKP", "RKP",
         dispatch, 5, 31, 0.0, 1.0 );
 
     ACES::Parameter* RAP = (ACES::Parameter*) new
-        Webots::Parameter("RAP", "Hip_Ankle_pitch_roll_3",
+        //Webots::Parameter("RAP", "Hip_Ankle_pitch_roll_3",
+        Webots::Parameter("RAP", "RAP",
         dispatch, 5, 31, 0.0, -1.0 );
 
     ACES::Parameter* RAR = (ACES::Parameter*) new
-        Webots::Parameter("RAR", "Foot_1",
+        //Webots::Parameter("RAR", "Foot_1",
+        Webots::Parameter("RAR", "RAR",
         dispatch, 5, 31, 0.0, 1.0 );
 
     dispatch->registerParameter(xx, webot);
