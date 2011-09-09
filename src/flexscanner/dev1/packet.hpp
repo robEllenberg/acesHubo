c6c18e62ff3692a3dbd569ca091451ef54eb2baf
Bob Sherbert <bob.sherbert@gmail.com>
2011-09-08 22:16:38 -0400

    Updated copyright notices

--- src/flexscanner/dev1/packet.hpp
+++ src/flexscanner/dev1/packet.hpp
@@ -1,6 +1,6 @@
-/* AUTORIGHTS
-    ACES: Abstracted Controls Engineering Software
-    Copyright (C) 2010 Robert Sherbert
+/* 
+    Conductor: High Degree of Freedom Robot Controller Framework
+    Copyright (C) 2010, 2011 Robert Sherbert
     bob.sherbert@gmail.com
 
     This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,15 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     If you are interested in licensing this software for commercial purposes
-    please contact the author.
+    please contact the author. The software can be licensed to you under
+    non-free license terms by the copyright holder.
+
+    As a courtesy to the author, and in the spirit of fair attribution, you are
+    asked to cite the following paper if any work based upon or utilizing this
+    framework is published in the scientific literature: 
+    Sherbert, Robert M. and Oh, Paul Y. "Conductor: A Controller Development
+    Framework for High Degree of Freedom Systems." Intelligent Robots and
+    Systems (IROS), 2011 IEEE/RSJ International Conference on. 
 */
 
 #ifndef __packet_h_
--- src/hubo/huboCanDS.hpp
+++ src/hubo/huboCanDS.hpp
@@ -1,3 +1,33 @@
+/* 
+    Conductor: High Degree of Freedom Robot Controller Framework
+    Copyright (C) 2010, 2011 Robert Sherbert
+    bob.sherbert@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    If you are interested in licensing this software for commercial purposes
+    please contact the author. The software can be licensed to you under
+    non-free license terms by the copyright holder.
+
+    As a courtesy to the author, and in the spirit of fair attribution, you are
+    asked to cite the following paper if any work based upon or utilizing this
+    framework is published in the scientific literature: 
+    Sherbert, Robert M. and Oh, Paul Y. "Conductor: A Controller Development
+    Framework for High Degree of Freedom Systems." Intelligent Robots and
+    Systems (IROS), 2011 IEEE/RSJ International Conference on. 
+*/
+
 #ifndef ACES_HUBOCANi_HPP
 #define ACES_HUBOCAN_HPP
 
