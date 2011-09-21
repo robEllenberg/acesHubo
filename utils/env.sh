86e228956a52da9c65c9f79739224d9df2036b7c
dasl <dasl@hubo-CAN.(none)>
2011-09-21 14:43:31 -0400

    Fixed diamondback build for ACES-PC

--- utils/env.sh
+++ utils/env.sh
@@ -2,17 +2,20 @@
 
 ACES_ROOT=$HOME/aces
 
-source /opt/ros/cturtle/setup.sh
-#RTT_COMPONENT_PATH=/home/bob/thesis/aces/build:/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
+source /opt/ros/diamondback/setup.bash
+#RTT_COMPONENT_PATH=/home/bob/thesis/aces/build:/opt/ros/diamondback/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
 export ROS_PACKAGE_PATH=$ACES_ROOT/build:$ROS_PACKAGE_PATH
-export RTT_COMPONENT_PATH=/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
+export RTT_COMPONENT_PATH=/opt/ros/diamondback/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
 
 export UTILS=$ACES_ROOT/utils
 export ACES_PATH=$ACES_ROOT/build/aces/bin:$UTILS
 export PATH=$ACES_PATH:$PATH
 
-#LD_LIBRARY_PATH=$ACES_ROOT/support/lib:/usr/local/webots/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/webots/lib/:/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib:/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib/orocos/plugins:$LD_LIBRARY_PATH
+#Webots library directories
+LD_LIBRARY_PATH=$ACES_ROOT/support/lib:/usr/local/webots/lib/:$LD_LIBRARY_PATH
+#Add additional library paths for diamondback, since ROS can't find them:
+LD_LIBRARY_PATH=/opt/ros/diamondback/stacks/orocos_toolchain_ros/rtt/install/lib/orocos/gnulinux/plugins:$LD_LIBRARY_PATH
+#LD_LIBRARY_PATH=/opt/ros/diamondback/stacks/orocos_toolchain_ros/ocl/install/lib/orocos:$LD_LIBRARY_PATH
 
 export WEBOTS_STDOUT=1
 export WEBOTS_STDERR=1
@@ -21,7 +24,6 @@ export WEBOTS_STDIN=1
 MATLABPATH=$ACES_ROOT/utils:$ACES_ROOT/utils/matlabIO:$MATLABPATH
 export MATLABPATH
 
-export ACES_BUILD=$ACES_ROOT/build/aces
 #stty -F /dev/ttyUSB0 raw
 #stty -F /dev/ttyUSB0 1000000
 #stty -F /dev/ttyUSB1 raw
