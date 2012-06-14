1240d1a2c6b0da1587baf0dd782851e20bd9df63
Duc Nguyen <ducleotide@gmail.com>
2012-06-14 16:05:48 -0400

    updated the utils/env.sh to correctly point to diamondback rather than cturtle version of ROS

--- utils/env.sh
+++ utils/env.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-ACES_ROOT=$HOME/aces
+ACES_ROOT=$HOME/workspace/hubo-proj/aces
+#ACES_ROOT=$PWD
+#ROS_DIR=/opt/ros/diamondback
+ROS_DIR=/opt/ros/diamondback
 
-source /opt/ros/diamondback/setup.bash
-#RTT_COMPONENT_PATH=/home/bob/thesis/aces/build:/opt/ros/diamondback/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
+source ${ROS_DIR}/setup.sh
+#RTT_COMPONENT_PATH=/home/bob/thesis/aces/build:/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
 export ROS_PACKAGE_PATH=$ACES_ROOT/build:$ROS_PACKAGE_PATH
-export RTT_COMPONENT_PATH=/opt/ros/diamondback/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
+export RTT_COMPONENT_PATH=${ROS_DIR}/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
+export OCL_PATH=${ROS_DIR}/stacks/orocos_toolchain_ros/ocl
 
 export UTILS=$ACES_ROOT/utils
 export ACES_PATH=$ACES_ROOT/build/aces/bin:$UTILS
 export PATH=$ACES_PATH:$PATH
 
-#Webots library directories
-LD_LIBRARY_PATH=$ACES_ROOT/support/lib:/usr/local/webots/lib/:$LD_LIBRARY_PATH
-#Add additional library paths for diamondback, since ROS can't find them:
-LD_LIBRARY_PATH=/opt/ros/diamondback/stacks/orocos_toolchain_ros/rtt/install/lib/orocos/gnulinux/plugins:$LD_LIBRARY_PATH
-#LD_LIBRARY_PATH=/opt/ros/diamondback/stacks/orocos_toolchain_ros/ocl/install/lib/orocos:$LD_LIBRARY_PATH
+#LD_LIBRARY_PATH=$ACES_ROOT/support/lib:/usr/local/webots/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/webots/lib/:${ROS_DIR}/stacks/orocos_toolchain_ros/rtt/install/lib:${OCL_PATH}/install/lib:${RTT_COMPONENT_PATH}/gnulinux/plugins:$LD_LIBRARY_PATH
 
 export WEBOTS_STDOUT=1
 export WEBOTS_STDERR=1
